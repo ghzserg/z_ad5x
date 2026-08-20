6d7e8574bfe5742471ef21df4df225d156ae939d
Sergei Rozhkov <zmod@zmod.link>
2026-08-20 17:02:35 +0500

    klipper_policy

---
+++ .shell/klipper_policy.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+# (C) 2026 ghzserg https://github.com/ghzserg/zmod
+
+. /opt/config/mod/.shell/0.sh
+
+MAX_RETRIES=30
+RETRY_COUNT=0
+
+get_klipper_pid() {
+    ps | grep klippy.py | grep -v grep | awk '{print $1}' | head -n 1
+}
+
+set_klipper_policy() {
+    while [ "$RETRY_COUNT" -le "$MAX_RETRIES" ]; do
+        PID=$(get_klipper_pid)
+
+        if [ -n "$PID" ]; then
+            echo "Process found, PID: $PID"
+
+            echo "Checking current policy for PID $PID:"
+            chrt -p "$PID"
+
+            chrt -f -p 50 "$PID" && echo "Successfully updated scheduling policy:"
+
+            chrt -p "$PID"
+            exit 0
+        else
+            if [ "$RETRY_COUNT" -eq 0 ]; then
+                echo "Process not found, waiting 2 seconds before retrying..."
+            else
+                echo "Retry #$RETRY_COUNT: Process has not started yet, still waiting..."
+            fi
+
+            sleep 2
+            RETRY_COUNT=$((RETRY_COUNT + 1))
+        fi
+    done
+
+    echo "Exceeded maximum number of retries ($MAX_RETRIES). Process failed to start."
+}
+
+mv ${MOD_CONF}/mod_data/log/klipper_policy.4.log ${MOD_CONF}/mod_data/log/klipper_policy.5.log 2>/dev/null
+mv ${MOD_CONF}/mod_data/log/klipper_policy.3.log ${MOD_CONF}/mod_data/log/klipper_policy.4.log 2>/dev/null
+mv ${MOD_CONF}/mod_data/log/klipper_policy.2.log ${MOD_CONF}/mod_data/log/klipper_policy.3.log 2>/dev/null
+mv ${MOD_CONF}/mod_data/log/klipper_policy.1.log ${MOD_CONF}/mod_data/log/klipper_policy.2.log 2>/dev/null
+mv ${MOD_CONF}/mod_data/log/klipper_policy.log ${MOD_CONF}/mod_data/log/klipper_policy.1.log 2>/dev/null
+
+set_klipper_policy >"${MOD_CONF}/mod_data/log/klipper_policy.log" 2>&1
